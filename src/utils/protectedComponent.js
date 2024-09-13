ab8bb0f498f3dbe15f32c9fa5092a755004e08ff
Lalo <lalo@physicslab.space>
2024-09-12 23:40:59 -0500

    no need for logging

--- src/utils/protectedComponent.js
+++ src/utils/protectedComponent.js
@@ -2,7 +2,6 @@ import { useAuth } from '../contexts/authContexts'
 
 
 const hasAllowedRole = (user, allowedRoles = []) => {
-  console.log('Checking roles for user:', user) // Debugging
   return allowedRoles.some(role => user && user[`is_${role}`])
 }
 
@@ -10,7 +9,6 @@ const ProtectedComponent = ({ allowedRoles = [], children }) => {
   const { user } = useAuth()
   
   if (allowedRoles.length > 0 && !hasAllowedRole(user, allowedRoles)) {
-    console.log('User does not have the required roles:', allowedRoles)
     return null
   }
   
